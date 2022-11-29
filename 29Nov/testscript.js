4e5141ba9d416622cee89322f4445ea94866ae97
ajayjha1 <ajayjha1886@gmail.com>
2022-11-29 15:45:58 +0530

    adding post Data function in testscript.js

--- 29Nov/testscript.js
+++ 29Nov/testscript.js
@@ -16,5 +16,29 @@ ShareApp.controller("testctr",[
         }
 
         $scope.getItems();
+
+        $scope.update() = function(){
+            var postData = {
+                __metadata: {'type': "SP.Data.TestEmployeeDataListItem"},
+                "FirstName": $scope.FirstName,
+                "LastName": $scope.LastName,
+                "Salary": $scope.Salary,
+            }
+
+            SharewebListService.AddListItemByListId(urlPath, listID, postData)
+            .then(function( Data ){
+                console.log('added succesfully')
+                alert('Success')
+                $scope.FirstName = ''
+                $scope.LastName = ''
+                $scope.Salary = ''
+                window.location.replace("https://smalsusinfolabs.sharepoint.com/sites/Dashboard/Prashant/Ajay/Pages/GetAndPostPage/GetAndPostPage.aspx");
+            },
+            function(error){
+                console.log(error);
+                alert("error");
+            }
+            )
+        }
     },
 ]);
